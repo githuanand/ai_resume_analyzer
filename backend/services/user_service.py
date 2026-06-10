49e7c1b4738a5123375c6356c7a895d425da87e3
githuanand <aj1001194@gmail.com>
2026-06-10 22:52:43 +0530

    backup before removing nextjs frontend

--- backend/services/user_service.py
+++ backend/services/user_service.py
@@ -1,9 +1,18 @@
 from repositories.user_repository import UserRepository
 
-from services.auth_service import (
-    hash_password,
-    verify_password,
-    create_access_token
+from passlib.context import CryptContext
+
+from jose import jwt
+from datetime import datetime, timedelta
+
+
+SECRET_KEY = "resume_saas_secret_key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
 )
 
 
@@ -34,7 +43,7 @@ class UserService:
 
         return self.user_repo.create(
             email=email,
-            password=hash_password(
+            password=self.hash_password(
                 password
             )
         )
@@ -56,7 +65,7 @@ class UserService:
                 "Invalid credentials"
             )
 
-        if not verify_password(
+        if not self.verify_password(
             password,
             user.password
         ):
@@ -64,19 +73,58 @@ class UserService:
                 "Invalid credentials"
             )
 
-        token = create_access_token(
+        return self.create_access_token(
             {
                 "sub": user.email
             }
         )
 
-        return token
+    def hash_password(
+        self,
+        password: str
+    ):
+        return pwd_context.hash(
+            password
+        )
+
+    def verify_password(
+        self,
+        plain_password: str,
+        hashed_password: str
+    ):
+        return pwd_context.verify(
+            plain_password,
+            hashed_password
+        )
+
+    def create_access_token(
+        self,
+        data: dict
+    ):
+
+        to_encode = data.copy()
+
+        expire = (
+            datetime.utcnow()
+            + timedelta(
+                minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+            )
+        )
+
+        to_encode.update(
+            {"exp": expire}
+        )
+
+        return jwt.encode(
+            to_encode,
+            SECRET_KEY,
+            algorithm=ALGORITHM
+        )
 
     def get_by_email(
         self,
         email: str
     ):
-
         return self.user_repo.get_by_email(
             email
         )
